c25c1ea80b96a5ddb96b10005b50eaeca37838c5
Travis Obregon <travisobregon@Traviss-MacBook-Pro.local>
2017-06-05 02:25:23 +0100

    Implement Send

--- Send/Send.cs
+++ Send/Send.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
 
-namespace Send
+class Send
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
         {
-            Console.WriteLine("Hello World!");
+            channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            string message = "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+            Console.WriteLine(" [x] Sent {0}", message);
         }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
     }
 }
